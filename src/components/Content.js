858f20005f91244f00e190af6c15e2b957375598
eutjin <91615591+eutjin@users.noreply.github.com>
2023-03-13 03:39:14 +0900

    fix hash links

--- src/components/Content.js
+++ src/components/Content.js
@@ -100,6 +100,16 @@ function Content({ setModal1, setModal2, setModal3, setModalResume }) {
 
   //   observer.observe(myRef.current);
   // }, []);
+  useEffect(()=>{
+    var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+    if (window.location.hash && isChrome) {
+        setTimeout(function () {
+            var hash = window.location.hash;
+            window.location.hash = "";
+            window.location.hash = hash;
+        }, 300);
+    }
+  },[])
 
   useEffect(() => {
     console.log("inview", inView1);
